fa2be070c11ff23204034d536dd127de5d8ed45b
RuslanTsitser <cicer_ruslan@mail.ru>
2023-10-09 14:35:49 +0300

    routing

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,3 +1,4 @@
+import 'package:counter_with_theme/core/presentation/router/app_router.dart';
 import 'package:counter_with_theme/core/services/local_storage_service/local_storage_service.dart';
 import 'package:counter_with_theme/core/services/location_service/location_service.dart';
 import 'package:counter_with_theme/core/services/permission/permission_service.dart';
@@ -9,21 +10,28 @@ class InjectionWidget extends InheritedWidget {
   final LocationService locationService;
   final PermissionService permissionService;
   final WeatherService weatherService;
+  final AppRouter appRouter;
   const InjectionWidget({
     super.key,
     required this.localStorageService,
     required this.locationService,
     required this.permissionService,
     required this.weatherService,
+    required this.appRouter,
     required super.child,
   });
 
-  static InjectionWidget? of(BuildContext context, {bool listen = false}) => listen
-      ? context.dependOnInheritedWidgetOfExactType<InjectionWidget>()
-      : context.findAncestorWidgetOfExactType<InjectionWidget>();
+  static InjectionWidget of(BuildContext context, {bool listen = false}) => listen
+      ? context.dependOnInheritedWidgetOfExactType<InjectionWidget>()!
+      : context.findAncestorWidgetOfExactType<InjectionWidget>()!;
 
   @override
   bool updateShouldNotify(InjectionWidget oldWidget) {
     return true;
   }
+
+  Future<void> init() async => await Future.wait([
+        localStorageService.init(),
+        weatherService.init(),
+      ]);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
+import 'package:counter_with_theme/core/presentation/router/app_router.dart';
+import 'package:counter_with_theme/core/services/local_storage_service/local_storage_service.dart';
+import 'package:counter_with_theme/core/services/location_service/location_service.dart';
+import 'package:counter_with_theme/core/services/permission/permission_service.dart';
+import 'package:counter_with_theme/core/services/weather_service/weather_service.dart';
+import 'package:counter_with_theme/injection.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    InjectionWidget(
+      localStorageService: LocalStorageServiceImpl(),
+      locationService: const LocationServiceImpl(),
+      permissionService: const PermissionServiceImpl(),
+      weatherService: const WeatherServiceImpl(),
+      appRouter: AppRouter(),
+      child: const MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    InjectionWidget.of(context).init().then((value) {
+      InjectionWidget.of(context).appRouter.go('/home');
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: InjectionWidget.of(context).appRouter,
     );
   }
 }
