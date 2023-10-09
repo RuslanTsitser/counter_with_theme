ac873cd0b4d9c31c6fe512bf382f8962bb9f3a1c
RuslanTsitser <cicer_ruslan@mail.ru>
2023-10-09 13:50:01 +0300

    services initial

---
+++ lib/core/services/base_init_service.dart
@@ -0,0 +1,3 @@
+abstract class BaseInitService {
+  Future<void> init();
+}
---
+++ lib/core/services/weather_service/weather_service.dart
@@ -0,0 +1,3 @@
+import 'package:counter_with_theme/core/services/base_init_service.dart';
+
+abstract class WeatherService implements BaseInitService {}
